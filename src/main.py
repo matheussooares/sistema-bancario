9785526f17171f6703dbf300c0178d6fc179e152
Matheus Soares <matheus.soares8890@gmail.com>
2024-06-15 15:04:37 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 def menu():
     view = """============ MENU =============\n
+    [nu] Novo usuário
     [d] Deposito
     [s] Sacar
     [e] Extrato
-    [x] Sair\n===============================
+    [x] Sair \n==>
     """
-    return view
+    return input(view)
 
-def extrato(extratos,saldo):
+def extrato(saldo, /,*,extratos):
     print('=========== EXTRATO ===========')
     if not extratos:
         print('Não foi realizado nenhuma movimentação')
@@ -17,59 +18,108 @@ def extrato(extratos,saldo):
     print('===============================')
 
 
-def deposito(saldo,extratos):
+def deposito(saldo,extratos,/):
 
     valor = float(input('Valor do deposito: '))
 
     if valor>0:
         saldo += valor
         extratos += f'Depósito: R$ {valor:.2f}\n'
-        print('Operação de deposito realiza com sucesso')
+        print('\n=== Operação de deposito realiza com sucesso ===')
     else:
-        print('Falha na operação: valor inválido')
+        print('\n@@@ Falha na operação: valor inválido @@@\n')
 
     return saldo,extratos
     
-def saque(saldo, extratos, num_saques):
+def saque(*,saldo, extratos, num_saques):
     LIMITE_SAQUES = 3
+    LIMITE = 500.00
+
     valor = float(input('Valor do saque: '))
 
     if valor > saldo:
-        print('Falha na operação: saldo insuficiente')
+        print('\n@@@ Falha na operação: saldo insuficiente @@@\n')
 
     elif num_saques >= LIMITE_SAQUES:
-        print('Falha na operação: excedeu número máximos de saque')
+        print('\n@@@ Falha na operação: excedeu número máximos de saque @@@\n')
     
-    elif valor > 0:
+    elif valor > 0 and  valor <= LIMITE:
         saldo -= valor
         extratos += f'Saque: R$ {valor:.2f}\n'
         num_saques +=1
-        print('Operação saque realiza com sucesso')
+        print('\n=== Operação saque realiza com sucesso ===\n')
     
     else:
-        print('Falha na operação: valor inválido')
+        print('\n@@@ Falha na operação: valor inválido @@@\n')
     
     return saldo,extratos,num_saques
 
+def usuario(usuarios):
+    cpf = input('Digite seu CPF: ')
+    user = filtro_usuario(cpf,usuarios)
 
+    if user == False:
+        print('n@@@ Falha no usuário: cpf existente @@@\n')
+    else:
+        nome = input('Nome completo: ')
+        dt_nascimento = input('Data de nascimento: ')
+        endereco = input('Endereço completo: ')
+
+        usuarios.append({'nome': nome,
+                         'data nascimento': dt_nascimento,
+                         'cpf': cpf,
+                         'endereco': endereco
+                         })
+        print('\n=== Criação de usuário realiza com sucesso ===\n')
+    
+    return usuarios
+
+def conta(usuarios,contas,numero_conta):
+    AGENCIA = '0001'
+    cpf = input('Digite seu CPF: ')
+    user = filtro_usuario(cpf,usuarios)
+
+    if user == False:
+        print('n@@@ Falha no usuário: cpf não encontrado @@@\n')
+    
+    else:
+        contas.append({'agencia':AGENCIA,
+                       'numero conta': numero_conta
+                       })
+    
+    return usuarios, contas
+
+
+
+def filtro_usuario(cpf,usuarios):
+    for usuario in usuarios:
+        if usuario['cpf'] == cpf:
+            return False
 
 def main():
     saldo = 0
     extratos = ''
     num_saques = 0
+    usuarios = []
+    contas = []
+    LIMITE = 500.00
 
     while True:
-        opc = input(menu())
-
+        opc = menu()
         if opc == 'd':
             saldo, extratos = deposito(saldo,extratos)
         elif opc == 's':
-            saldo, extratos, num_saques = saque(saldo, extratos, num_saques)
+            saldo, extratos, num_saques = saque(
+                                                saldo = saldo,
+                                                extratos = extratos, 
+                                                num_saques = num_saques)
         elif opc == 'e':
-            extrato(extratos,saldo)
+            extrato(saldo, extratos = extratos)
         elif opc == 'x':
             break
+        elif opc == 'nu':
+            usuarios = usuario(usuarios)
         else:
-            print('Digite um valor válido')
+            print('\n@@@ Falha na operação: digite um valor válido @@@\n')
 
 main()
